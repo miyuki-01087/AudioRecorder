aba3356efa2d97310175af4bf42b314f9077b826
miyuki-98312 <ritzft@gmail.com>
2021-05-30 20:17:38 +0900

    anyway

---
+++ AudioRecorder/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace AudioRecorder
+{
+    public partial class Form1 : Form
+    {
+        private bool is_recording = false;
+        private Record reco;
+        public Form1()
+        {
+            this.Text = "Recorder";
+            InitializeComponent();
+            reco = new Record();
+            label_folder.Text = System.Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            if (is_recording)
+            {
+                btn_start.Text = "Start Recording";
+                is_recording = false;
+                reco.QuitRecording();
+                chk_silence.Enabled = true;
+            }
+            else
+            {
+                string filename = box_filename.Text;
+                btn_start.Text = "Stop Recording";
+                reco.setup(label_folder.Text, filename, chk_silence.Checked);
+                is_recording = true;
+                chk_silence.Enabled = false;
+            }
+        }
+
+        private void btn_select_folder_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+
+
+            commonOpenFileDialog.Title = "フォルダを選択してください。";
+            commonOpenFileDialog.InitialDirectory = @"C:";
+            commonOpenFileDialog.IsFolderPicker = true;
+
+
+            if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                label_folder.Text = commonOpenFileDialog.FileName;
+            }
+            else
+            {
+                label_folder.Text = "キャンセルされました。";
+            }
+        }
+    
+    }
+}
---
+++ AudioRecorder/Record.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.Threading;
+using NAudio.Wave.SampleProviders;
+
+namespace AudioRecorder
+{
+    class Record
+    {
+        private bool isRecording = false;
+        private WasapiLoopbackCapture capture;
+        private WaveOutEvent wo = new WaveOutEvent();
+        public Record()
+        {
+            capture = new WasapiLoopbackCapture();
+        }
+
+        public void setup(string folderPath, string fileName, bool isRecordSilence)
+        {
+            if (isRecording)
+            {
+                throw new InvalidOperationException("Already in Recording!");
+            }
+            else
+            {
+                var outputFolder = Path.Combine(folderPath);
+                var outputFilePath = Path.Combine(outputFolder, fileName + ".wav");
+                var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
+
+
+                capture.DataAvailable += (s, a) =>
+                {
+                    writer.Write(a.Buffer, 0, a.BytesRecorded);
+                };
+
+                capture.RecordingStopped += (s, a) =>
+                {
+                    writer.Dispose();
+                    writer = null;
+                    capture.Dispose();
+                };
+
+                Task task_record = Task.Run(() => {
+                    StartRecording();
+                });
+                isRecording = true;
+
+                if (isRecordSilence)
+                {
+                    Task task_silence = Task.Run(() => {
+                        StartSine();
+                    });
+                }
+            }
+        }
+
+        private void StartSine()
+        {
+            var sinesetup = new SignalGenerator()
+            {
+                Gain = 0.0,
+                Frequency = 440,
+                Type = SignalGeneratorType.Sin
+            };
+
+            wo.Init(sinesetup);
+            wo.Play();
+            while (wo.PlaybackState == PlaybackState.Playing)
+            {
+                Thread.Sleep(500);
+            }
+        }
+
+        private void QuitSine()
+        {
+            wo.Stop();
+        }
+
+        public void QuitRecording()
+        {
+            isRecording = false;
+            capture.StopRecording();
+            QuitSine();
+        }
+
+        private void StartRecording()
+        {
+            capture.StartRecording();
+            while (capture.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+
+}
