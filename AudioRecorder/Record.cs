03e29c67274c831f1671e75cf174b0d0fc422f43
miyuki-98312 <ritzft@gmail.com>
2021-06-04 22:43:33 +0900

    Just fixed using discription

--- AudioRecorder/Record.cs
+++ AudioRecorder/Record.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using NAudio.Wave;
-using NAudio.CoreAudioApi;
 using System.Threading;
 using NAudio.Wave.SampleProviders;
 using System.Windows.Forms;
